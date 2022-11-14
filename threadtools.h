86c27fdaa93ac3190276536bb642d70c5cf41683
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2022-11-14 16:26:02 +0800

    hw3 released :3

---
+++ threadtools.h
@@ -0,0 +1,55 @@
+#ifndef THREADTOOL
+#define THREADTOOL
+#include <setjmp.h>
+#include <sys/signal.h>
+
+#define THREAD_MAX 16  // maximum number of threads created
+#define BUF_SIZE 512
+struct tcb {
+    int id;  // the thread id
+    jmp_buf environment;  // where the scheduler should jump to
+    int arg;  // argument to the function
+    int fd;  // file descriptor for the thread
+    char buf[BUF_SIZE];  // buffer for the thread
+    int i, x, y;  // declare the variables you wish to keep between switches
+};
+
+extern int timeslice;
+extern jmp_buf main_buf, sched_buf;
+extern struct tcb *ready_queue[THREAD_MAX], *waiting_queue[THREAD_MAX];
+/*
+ * rq_size: size of the ready queue
+ * rq_current: current thread in the ready queue
+ * wq_size: size of the waiting queue
+ */
+extern int rq_size, rq_current, wq_size;
+/*
+* base_mask: blocks both SIGTSTP and SIGALRM
+* tstp_mask: blocks only SIGTSTP
+* alrm_mask: blocks only SIGALRM
+*/
+extern sigset_t base_mask, tstp_mask, alrm_mask;
+/*
+ * Use this to access the running thread.
+ */
+#define RUNNING (ready_queue[rq_current])
+
+void sighandler(int signo);
+void scheduler();
+
+#define thread_create(func, id, arg) {\
+}
+
+#define thread_setup(id, arg) {\
+}
+
+#define thread_exit() {\
+}
+
+#define thread_yield() {\
+}
+
+#define async_read(count) ({\
+})
+
+#endif // THREADTOOL
